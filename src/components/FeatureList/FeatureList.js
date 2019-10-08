e70f6997a12917586ba5ac71095fc1bb042f04b1
Maxim Onyskiv <maxim.onyskiv@gmail.com>
2019-10-08 18:30:42 +0300

    FeatureList added

---
+++ src/components/FeatureList/FeatureList.js
@@ -0,0 +1,17 @@
+import React from "react"
+import styles from "./styles.module.css"
+import features from "../../utils/features"
+import { Inner } from "../Container"
+import FeatureCard from "../FeatureCard"
+
+const FeatureList = () => (
+  <Inner>
+    <div className={styles.FeatureList}>
+      {features.map(({ id, image, alt, text }) => (
+        <FeatureCard key={id} img={image} alt={alt} text={text} />
+      ))}
+    </div>
+  </Inner>
+)
+
+export default FeatureList
