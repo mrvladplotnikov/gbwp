78b52f46a26dc96552fc98d413c5126226f5a184
Maxim Onyskiv <maxim.onyskiv@gmail.com>
2019-10-08 18:29:10 +0300

    config changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `VP Production `,
+    description: `Мы – команда людей, которые создают музыку, звуки и продюсируют дикторское озвучивание на грани чистого творчества и функциональности.`,
+    author: `@tarotum`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
