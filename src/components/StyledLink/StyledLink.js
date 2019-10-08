44bb7f503b44558e3705478038ab5beb1003874a
Maxim Onyskiv <maxim.onyskiv@gmail.com>
2019-10-08 16:19:19 +0300

    StyledLink added

---
+++ src/components/StyledLink/StyledLink.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+
+import styles from "./styles.module.css"
+
+const StyledLink = ({ children, to, ...attrs }) => {
+  return (
+    <div className={styles.wrapper}>
+      <Link className={styles.StyledLink} to={to} {...attrs}>
+        {children}
+      </Link>
+      <div className={styles.arrowWrapper} aria-hidden="true">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="73"
+          height="9"
+          fill="none"
+          className={styles.arrow}
+        >
+          <path stroke="#747474" d="M0 4.5h72m0 0L67.5 1M72 4.5L67.5 8" />
+        </svg>
+      </div>
+    </div>
+  )
+}
+
+StyledLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.string,
+}
+
+StyledLink.defaultProps = {
+  children: "Link",
+}
+
+export default StyledLink
