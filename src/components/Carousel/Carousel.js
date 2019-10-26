d51786e224fda466c7fa659945509b2b60e242ee
tarotum <onyskiv.maxim@gmail.com>
2019-10-26 18:53:11 +0300

    added new carousel package

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,36 @@
+import React from "react"
+import PropTypes from "prop-types"
+import ReactAliceCarousel from "react-alice-carousel"
+import "react-alice-carousel/lib/alice-carousel.css"
+import "./styles.css"
+
+const Carousel = ({ children }) => (
+  <ReactAliceCarousel
+    buttonsDisabled
+    mouseDragEnabled
+    fadeOutAnimation
+    infinite={false}
+    responsive={{
+      0: {
+        items: 1,
+      },
+      445: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      980: {
+        items: 5,
+      },
+    }}
+  >
+    {children}
+  </ReactAliceCarousel>
+)
+
+Carousel.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Carousel
