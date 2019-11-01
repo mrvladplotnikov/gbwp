a0475fe2ad36db24608f2b85dbc66ad4cff354e4
tarotum <onyskiv.maxim@gmail.com>
2019-11-01 23:29:56 +0200

    added className prop to containers

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,16 +1,23 @@
 import React from "react"
 import PropTypes from "prop-types"
+import classNames from "classnames"
 import styles from "./styles.module.css"
 
-const Outer = ({ children }) => <div className={styles.Outer}>{children}</div>
+const Outer = ({ className = "", children }) => (
+  <div className={classNames(styles.Outer, className)}>{children}</div>
+)
 
 Outer.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.node.isRequired,
 }
 
-const Inner = ({ children }) => <div className={styles.Inner}>{children}</div>
+const Inner = ({ className = "", children }) => (
+  <div className={classNames(styles.Inner, className)}>{children}</div>
+)
 
 Inner.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.node.isRequired,
 }
 
