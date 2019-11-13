f5a9b9939068f445706053b81115e4a83313f9bc
tarotum <onyskiv.maxim@gmail.com>
2019-11-14 00:42:25 +0200

    added home page translations

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -1,11 +1,35 @@
-import React from "react"
+import React, { useRef } from "react"
 import Layout from "../layouts"
-import FeatureList from "../components/FeatureList/FeatureList"
+import Hero from "../components/lendingSections/Hero"
+import About from "../components/lendingSections/About"
+import Why from "../components/lendingSections/Why"
+import Services from "../components/lendingSections/Services"
+import ChosenWorks from "../components/lendingSections/ChosenWorks"
+import Contacts from "../components/lendingSections/Contacts"
+import SectionWithText from "../components/SectionWithText"
 
-const Home = () => (
-  <Layout>
-    <FeatureList />
-  </Layout>
-)
+const scrollToRef = ref => window.scrollTo(0, ref.current.offsetTop)
+
+const Home = () => {
+  const portfolioRef = useRef(null)
+  const executeScroll = () => scrollToRef(portfolioRef)
+
+  return (
+    <Layout>
+      <Hero onClick={executeScroll} />
+      <About />
+      <Why />
+      <Services />
+      <ChosenWorks portfolioRef={portfolioRef} />
+      <SectionWithText link="/" linkLabel="Наши ценности">
+        <h3>
+          3 помощью аудио мы вдохновляем слушателей по обе стороны медиапроектов
+          на новый опыт и раскрытие собственного творческого потенциала.
+        </h3>
+      </SectionWithText>
+      <Contacts />
+    </Layout>
+  )
+}
 
 export default Home
