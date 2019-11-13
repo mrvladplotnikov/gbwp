ffeefcac3875349abd4eb2269aeba59e3df0a555
tarotum <onyskiv.maxim@gmail.com>
2019-11-14 00:41:48 +0200

    added work template

---
+++ src/utils/parseContent.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Img from "gatsby-image"
+import Parser from "html-react-parser"
+
+export default (content, media) =>
+  Parser(content, {
+    replace: domNode => {
+      if (domNode.name === "img") {
+        let image = media.find(
+          m => m.node.wordpress_id === parseInt(domNode.attribs["data-id"], 10)
+        )
+
+        if (image)
+          return <Img fluid={image.node.localFile.childImageSharp.fluid} />
+      }
+    },
+  })
