364323ac59d6a510666904ac2f99532daefcb24c
tarotum <onyskiv.maxim@gmail.com>
2020-05-25 19:06:28 +0300

    portfolio btn updated

--- src/components/PortfolioLink/PortfolioLink.js
+++ src/components/PortfolioLink/PortfolioLink.js
@@ -6,6 +6,7 @@ import { FormattedMessage } from "react-intl"
 
 const PortfolioLink = ({ link = "/portfolio" }) => (
   <Link to={link} className={styles.PortfolioLink}>
+    <span className={styles.line}></span>
     <FormattedMessage id="portfolioLabel" />
   </Link>
 )
